392708bf49e170c7dd675119759229f0849ce0a9
Saad Bash <saadbashdev@gmail.com>
2025-03-21 21:27:40 -0400

    Guestbook (#3)
    - GitHub OAuth setup
    - New guestbook page
    - QOL changes

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/posts', title: 'Posts' },
   { href: '/tags', title: 'Tags' },
+  { href: '/guestbook', title: 'Guestbook' },
   { href: '/about', title: 'About' },
 ]
 
